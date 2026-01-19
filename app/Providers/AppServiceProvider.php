1e7faf31f218c02c078c841ac29d0e86ed65dd03
Alexandru Muhai <naurabb@naurabb.home>
2026-01-19 16:06:19 +0100

    naprawa https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(str_contains(config('app.url'), 'https://')) {
-            URL::forceScheme('https');
-        }
+        \Illuminate\Support\Facades\URL::forceScheme('https');
     }
 }
